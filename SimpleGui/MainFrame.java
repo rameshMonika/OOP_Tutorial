22f5b88c2676184d5ea398f2cf997fd97f7fa1a7
rameshMonika <rameshmonika11@gmail.com>
2024-04-11 22:06:41 +0800

    commit

--- SimpleGui/MainFrame.java
+++ SimpleGui/MainFrame.java
@@ -1,4 +1,4 @@
-package SimpleGui;
+
 
 import java.awt.*;
 import java.awt.event.*;
@@ -6,82 +6,26 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-    final private Font mainFont = new Font("Segoe print", Font.BOLD, 18);
-    JTextField tfFirstName, tfLastName;
-    JLabel lbWelcome;
+   
+    public void initialize(){
+        JFrame f1= new JFrame("HELLO");
+        f1.setSize(600,600);
+        JLabel l1= new JLabel();
+        l1.setText("Hello");
 
-    public void initialize() {
-        JLabel lbFirstName = new JLabel("First Name");
-        lbFirstName.setFont(mainFont);
+        f1.add(l1);
 
-        tfFirstName = new JTextField();
-        tfFirstName.setFont(mainFont);
+        f1.setVisible(true);
+        
 
-        JLabel lbLastName = new JLabel("Last Name");
-        lbLastName.setFont(mainFont);
-
-        tfLastName = new JTextField();
-        tfLastName.setFont(mainFont);
-
-        JButton btnOk = new JButton("OK");
-        btnOk.setFont(mainFont);
-        btnOk.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                String firstName = tfFirstName.getText();
-                String lastName = tfLastName.getText();
-                lbWelcome.setText("Hi " + firstName + " " + lastName);
-            }
-
-        });
-
-        JButton btnClear = new JButton("Clear");
-        btnClear.setFont(mainFont);
-        btnClear.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                tfFirstName.setText("");
-                tfLastName.setText("");
-                lbWelcome.setText("");
-            }
-
-        });
-
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.add(btnOk);
-        buttonsPanel.add(btnClear);
-
-        JPanel formPanel = new JPanel();
-        formPanel.setLayout(new GridLayout(4, 1, 5, 5));
-        formPanel.add(lbFirstName);
-        formPanel.add(tfFirstName);
-        formPanel.add(lbLastName);
-        formPanel.add(tfLastName);
-
-        lbWelcome = new JLabel();
-        lbWelcome.setFont(mainFont);
-
-        JFrame frame = new JFrame();
-        frame.add(formPanel, BorderLayout.NORTH);
-        frame.add(lbWelcome, BorderLayout.CENTER);
-        frame.add(buttonsPanel, BorderLayout.SOUTH);
-
-        add(frame);
-
-        setTitle("HI!");
-        setLayout(new BorderLayout());
-        setBackground(new Color(128, 128, 255));
-        setSize(new Dimension(500, 600));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
     }
 
     public static void main(String[] args) {
-        gui gui = new gui();
+        MainFrame gui = new MainFrame();
         gui.initialize();
     }
 
+  
+
 }
+
